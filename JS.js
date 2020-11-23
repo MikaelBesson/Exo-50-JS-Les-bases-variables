587ce3d009e18bcb4cb530a1765d09dbe0d920fc
BessonMikael <besson.mikael04@gmail.com>
2020-11-23 11:45:07 +0100

    exo 50

---
+++ JS.js
@@ -0,0 +1,14 @@
+let nom = "nom:doe";
+let prenom ="prenom:john";
+let ville = "ville:new-york";
+
+
+
+alert(nom +"\n" + prenom +"\n" + ville);
+
+let entrez = prompt("entrez votre nom");
+
+alert(entrez);
+
+
+
